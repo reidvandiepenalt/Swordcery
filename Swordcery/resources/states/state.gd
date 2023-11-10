cf6daa0663ac28f69a779a83e17cb407baba5af9
Reid VanDiepen <reidvandie@gmail.com>
2023-11-10 18:28:26 -0500

    set up basic state machine code

---
+++ Swordcery/resources/states/state.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name State
+
+signal Transitioned
+
+func Enter_State():
+	pass
+
+func Exit_State():
+	pass
+
+func Update_State(_delta: float):
+	pass
+
+func Physics_Update_State(_delta: float):
+	pass
