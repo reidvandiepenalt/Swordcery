7d3e421ccb4888463baa5e106120ae4bf4cb9bda
reidvandiepenalt <reidvandie@gmail.com>
2024-01-17 17:42:30 -0500

    projectiles only move when target is set. add timed despawn to player projectile

--- Swordcery/player/projectiles/player_projectile.gd
+++ Swordcery/player/projectiles/player_projectile.gd
@@ -2,13 +2,25 @@ extends "res://resources/Hitbox.gd"
 
 @export var SPEED := 100
 @export var TARGET := Vector3.ZERO
+@export var timeout := 20.0
+
+var despawn_timer := 0
+var has_target := false
 
 func set_target(target: Vector3):
+	has_target = true
 	TARGET = target
 	look_at(TARGET, Vector3.UP)
 
 func _physics_process(delta):
+	if !has_target:
+		return
+	
 	global_position -= SPEED * self.basis.z * delta
+	
+	despawn_timer += delta
+	if (despawn_timer > timeout):
+		destroy()
 
 func destroy():
 	queue_free()
