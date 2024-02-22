8f6883e6e5afd0478bf8b1191d681f23add6f07c
Reid VanDiepen <reidvandie@gmail.com>
2024-02-22 11:09:45 -0500

    setup aerial wander script

--- Swordcery/resources/states/aerial_wander.gd
+++ Swordcery/resources/states/aerial_wander.gd
@@ -2,37 +2,43 @@ extends State
 class_name AerialWander
 
 @export var enemy: EnemyBase
+@export var squared_distance_to_reach_target := 1.0
+@export var wander_range := 10.0
 
-var level : Level
-var set_first_node = false
+var home : Vector3
+var target_position : Vector3
 
 func Enter_State():
-	level = get_tree().get_first_node_in_group("Level")
+	Get_New_Home_Position()
+	Get_New_Wander_Target()
 
 func Exit_State():
 	super.Exit_State()
 
 func Update_State(_delta: float):
-	if !set_first_node:
-		set_first_node = true
-		Get_New_Wander_Node()
+	super.Update_State(_delta)
 
 func Physics_Update_State(_delta: float):
 	if enemy:
 		if (enemy.player.global_position - enemy.global_position).length()  < enemy.chase_range:
 			Transitioned.emit(self, transitionState.name)
 			return
-
+		
 		var current_location = enemy.global_transform.origin
-		var next_location = enemy.nav_agent.get_next_path_position()
-		var new_velocity = (next_location - current_location).normalized() * enemy.SPEED * 0.75 * enemy.speed_modifier
+		if(current_location.distance_squared_to(target_position) <= squared_distance_to_reach_target):
+			Get_New_Wander_Target()
+		
+		var new_velocity = (target_position - current_location).normalized() * enemy.SPEED * 0.75 * enemy.speed_modifier
 	
 		enemy.velocity.x = move_toward(enemy.velocity.x, new_velocity.x, enemy.SPEED * enemy.speed_modifier)
 		enemy.velocity.z = move_toward(enemy.velocity.z, new_velocity.z, enemy.SPEED * enemy.speed_modifier)
+		enemy.velocity.y = move_toward(enemy.velocity.y, new_velocity.y, enemy.SPEED * enemy.speed_modifier)
 
-func Get_New_Wander_Node():
-	var random_index = randi()%level.nav_mesh.get_vertices().size()
-	enemy.update_target_location(level.nav_mesh.get_vertices()[random_index])
+func Get_New_Wander_Target():
+	var random_radian1 = randf_range(0, TAU)
+	var random_radian2 = randf_range(0, TAU)
+	var random_dist = randf_range(0.25 * wander_range, wander_range)
+	target_position = home + Vector3(cos(random_radian1), sin(random_radian2), sin(random_radian1)) * random_dist
 
-func _on_navigation_agent_3d_navigation_finished():
-	Get_New_Wander_Node()
+func Get_New_Home_Position():
+	home = enemy.global_position
